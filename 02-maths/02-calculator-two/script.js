e013310a7e021af7542418b8517bb1bf716074d4
ChristianJadot <christian.jadot@gmail.com>
2021-02-11 17:06:08 +0100

    02-calculator two script upload

---
+++ 02-maths/02-calculator-two/script.js
@@ -0,0 +1,40 @@
+/* becode/javascript
+ *
+ * /02-maths/02-calculator-two/script.js - 2.2: calculatrice (2)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    // to get the value of an input: document.getElementById("element-id").value
+
+    const performOperation = operation => {
+        valeur1 = eval(document.getElementById("op-one").value);
+        valeur2 = eval(document.getElementById("op-two").value);
+        switch (operation) {
+            case "addition" :
+                alert (valeur1 + valeur2);
+                break;
+            case "substraction" :
+                alert (valeur1 - valeur2);
+                break;
+            case "multiplication" :
+                alert (valeur1 * valeur2);
+                break;
+            case "division" :
+                alert (valeur1 / valeur2);
+                break;
+        };
+    };
+
+    Array.from(document.querySelectorAll("button.operator")).forEach($btn =>
+        $btn.addEventListener(
+            "click",
+            () => (performOperation($btn.id), false),
+        ),
+    );
+})();
